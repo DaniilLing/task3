0a4d323a75e42b70168ed31e5eef60113abd25d4
DaniilLing <136614439+DaniilLing@users.noreply.github.com>
2023-06-22 17:44:54 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+System.Console.WriteLine($"Числo {num}  четное");
+else System.Console.WriteLine($"Число: {num} нечетное");
